fd6dffc9e293eb19bd97d408dd2ad32c8f11bcee
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2026-01-06 05:54:40 -0500

    feat(Header): add Header Component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router";
+import { ShoppingCart, Home, Store } from "lucide-react";
+import styles from './Header.module.css'
+
+
+export default function Header() {
+  
+  return (
+    <header className={`${styles.header} row-btw`}>
+      <div className="row">
+        <div className="logo">
+          <h1 className={styles.heading}>Who'sStore</h1>
+        </div>
+        <nav>
+          <ul className="nav-list row">
+            <li className="nav__item">
+              <Link to="/" className={`${styles.navLink}`}>Home</Link>
+            </li>
+            <li className="nav__item">
+              <Link to="/" className={`${styles.navLink}`}>Products</Link>
+            </li>
+            <li className="nav__item">
+              <Link to="/" className={`${styles.navLink}`}>Cart</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="">
+        <nav className="nav-icons">
+          <ul className="nav-list row">
+            <li className={`${styles.navIcon}`}>
+              <Link to="/" className={`${styles.navIconLink}`}>{<Home />}</Link>
+            </li>
+            <li className={`${styles.navIcon}`}>
+              <Link to="/" className={`${styles.navIconLink}`}>
+                <Store />
+              </Link>
+            </li>
+            <li className={`${styles.navIcon}`}>
+              <Link to="/" className={`${styles.navIconLink}`}>{<ShoppingCart />}</Link>
+            </li>
+          </ul>
+        </nav>
+        
+      </div>
+    </header>
+  );
+}
