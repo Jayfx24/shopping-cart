ab1db32daa7d7b37bb164ff41a693a7911360e11
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2026-01-08 04:54:56 -0500

    feat(useSortProduct): sort products by category

--- src/hooks/useSortProduct.jsx
+++ src/hooks/useSortProduct.jsx
@@ -1,27 +1,55 @@
 import { useState, useEffect } from "react";
 import fetchAllProducts from "../services/api";
+import data from "../test.json";
 
 export default function useSortProduct() {
   const [products, setProducts] = useState();
-  const url = "https://dummyjson.com/products?limit=0";
+
+  // const url = "https://dummyjson.com/products?limit=0";
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetchAllProducts(url);
-        console.log(response)
-        setProducts(response);
+        // const response = await fetchAllProducts(url);
+        const obj = {};
+        const menObj = {};
+        const womenObj = {};
+
+        Object.values(data.products).forEach((item) => {
+          const product = item;
+          const isMen = product.category.startsWith("men");
+          const isWomen = product.category.startsWith("women");
+
+          if (isMen)
+            Object.hasOwn(menObj, product.category)
+              ? menObj[product.category].push(product)
+              : (menObj[product.category] = [product]);
+
+          if (isWomen)
+            Object.hasOwn(womenObj, product.category)
+              ? womenObj[product.category].push(product)
+              : (womenObj[product.category] = [product]);
+
+          if (!isMen && !isWomen)
+            Object.hasOwn(obj, product.category)
+              ? obj[product.category].push(product)
+              : (obj[product.category] = [product]);
+        });
+
+        setProducts({
+          men: { ...menObj },
+          women: { ...womenObj },
+          others: { ...obj },
+        });
       } catch (err) {
         console.log(err);
       }
     };
     fetchProducts();
-}, []);
+  }, []);
 
-
-
-
-  return{
-    products
-  }
+  return {
+    products,
+  };
 }
+
