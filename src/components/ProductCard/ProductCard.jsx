453d1f30b0599368d949e06bfe06d7f9d8a4bbe7
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2026-01-08 05:59:20 -0500

    feat(ProductCard): add product card component

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,18 @@
+import styles from "./ProductCard.module.css";
+
+export default function ProductCard({ category, price, title, images }) {
+  const cleanCategory = category?.split("-").join(" ");
+
+  return (
+    <article className={styles.card}>
+      <div className="img-wrapper">
+        <img src={images?.[0]} alt={title + "image"} />
+      </div>
+      <div className={styles.content}>
+        <span className={styles.category}>{cleanCategory.toUpperCase()}</span>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.price}>${price}</h2>
+      </div>
+    </article>
+  );
+}
