7153fea4e7b7b1a1ec53dc37e8677b3e1eae675d
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2026-01-16 04:59:46 -0500

    refactor(Shop): improve filter logic

--- src/components/ui/Checkbox/Checkbox.jsx
+++ src/components/ui/Checkbox/Checkbox.jsx
@@ -1,4 +1,5 @@
 export default function Checkbox({ id, label, onChange, isChecked }) {
+
   return (
     <label htmlFor={label}>
       <input
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -7,52 +7,63 @@ export default function Shop() {
   const products = useOutletContext();
   const [currentCategory, setCurrentCategory] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState({});
-  const [categoryChecked, setCategoryChecked] = useState(false);
-  const [checkedStatus, setCheckedStatus] = useState(Object.keys(products));
+  const [checkedStatus, setCheckedStatus] = useState({});
+
+  // if unchecked remove from selected Category
+  // if main category is selected all child should be selected if one is then selected all should be unselected
 
   function handleChange(e, category, subCategory = null) {
     const target = e.target;
-
+    console.log(checkedStatus);
     //  if category is unchecked remove  its children from selectedCategory
     if (!subCategory && !target.checked) {
       const categoryItems = Object.keys(products[category]);
-      const newObj = Object.fromEntries(
+      const newSelected = Object.fromEntries(
         Object.entries(selectedCategory).filter(
           ([key]) => !categoryItems.includes(key)
         )
       );
-      console.log(categoryItems);
-      console.log(newObj);
-      setSelectedCategory(newObj);
-      setCheckedStatus((prev) => ({ ...prev, [category]: true }));
+
+      // uncheck subs
+      const categorySubs = Object.fromEntries(
+        Object.keys(products[category]).map((i) => [i, false])
+      );
+
+      setSelectedCategory(newSelected);
+      setCheckedStatus((prev) => ({ ...prev, ...categorySubs }));
       return;
     }
 
     // add category children
     if (!subCategory) {
-      console.log(products[category]);
+      const categorySubs = Object.fromEntries(
+        Object.keys(products[category]).map((i) => [i, true])
+      );
+
+      setCheckedStatus((prev) => ({ ...prev, ...categorySubs }));
       setSelectedCategory((prev) => ({
         ...prev,
         ...{ ...products[category] },
       }));
-      setCheckedStatus((prev) => ({ ...prev, [category]: false }));
-
       return;
     }
 
-    // if subCategory is checked / unchecked update selectedCategory according
-    const newObj = target.checked
-      ? { ...selectedCategory, [subCategory]: products[category][subCategory] }
-      : Object.fromEntries(
-          Object.entries(selectedCategory).filter(([k]) => k !== subCategory)
-        );
+    let newSelected;
+    if (target.checked) {
+      newSelected = {
+        ...selectedCategory,
+        [subCategory]: products[category][subCategory],
+      };
 
-    setSelectedCategory(newObj);
+      setCheckedStatus((prev) => ({ ...prev, [subCategory]: true }));
+    } else {
+      newSelected = Object.fromEntries(
+        Object.entries(selectedCategory).filter(([k]) => k !== subCategory)
+      );
+      setCheckedStatus((prev) => ({ ...prev, [subCategory]: false }));
+    }
 
-    // if unchecked remove from selected Category
-    // if main category is selected all child should be selected if one is then selected all should be unselected
-    console.log(newObj);
-    // setCurrentCategory(newArr);
+    setSelectedCategory(newSelected);
   }
 
   return (
@@ -71,14 +82,18 @@ export default function Shop() {
                 </legend>
 
                 <div className="filter__sub">
-                  {Object.entries(values).map(([k, v]) => (
-                    <Checkbox
-                      key={`${category}-${k}`}
-                      label={k}
-                      id={k}
-                      onChange={(e) => handleChange(e, category, k)}
-                    />
-                  ))}
+                  {Object.entries(values).map(([subCategory, _]) => {
+                    const isSelected = checkedStatus[subCategory] ?? false;
+                    return (
+                      <Checkbox
+                        key={`${category}-${subCategory}`}
+                        label={subCategory}
+                        id={subCategory}
+                        onChange={(e) => handleChange(e, category, subCategory)}
+                        isChecked={isSelected}
+                      />
+                    );
+                  })}
                 </div>
               </fieldset>
             );
