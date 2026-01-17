4256f474638572c5826f0cc398647eca94008c0a
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2026-01-17 05:16:10 -0500

    refactor(useSortProducts): refactor hooks/useSortProducts to loaders/productsLoaders for routes

---
+++ src/loaders/productsLoader.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import fetchAllProducts from "../services/api"
+import data from "../test.json";
+
+export default async function productsLoader() {
+  const obj = {};
+  const menObj = {};
+  const womenObj = {};
+  try {
+    // const response = await fetchAllProducts(url);
+
+    Object.values(data.products).forEach((item) => {
+      const product = item;
+      const isMen = product.category.startsWith("men");
+      const isWomen = product.category.startsWith("women");
+
+      if (isMen)
+        Object.hasOwn(menObj, product.category)
+          ? menObj[product.category].push(product)
+          : (menObj[product.category] = [product]);
+
+      if (isWomen)
+        Object.hasOwn(womenObj, product.category)
+          ? womenObj[product.category].push(product)
+          : (womenObj[product.category] = [product]);
+
+      if (!isMen && !isWomen)
+        Object.hasOwn(obj, product.category)
+          ? obj[product.category].push(product)
+          : (obj[product.category] = [product]);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(obj)
+
+  return {
+    sortedProducts: {
+      men: menObj,
+      women: womenObj,
+      others: obj,
+    },
+    unsortedProducts: data.products,
+  };
+}
