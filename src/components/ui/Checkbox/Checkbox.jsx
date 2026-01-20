f9adad080a5afc6a40ae4f7043e7aa6a86a3cf6e
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2026-01-20 05:41:38 -0500

    refactor(Checkbox): move input position

--- src/components/ui/Checkbox/Checkbox.jsx
+++ src/components/ui/Checkbox/Checkbox.jsx
@@ -1,11 +1,16 @@
-import styles from './Checkbox.module.css'
-
+import styles from "./Checkbox.module.css";
 
 export default function Checkbox({ id, label, onChange, isChecked }) {
   return (
     <li className={styles.checkBox}>
-      <input type="checkbox" id={id} onChange={onChange} checked={isChecked} />
-      <label htmlFor={label}>{label}</label>
+      <label htmlFor={id}>{label}</label>
+      <input
+        type="checkbox"
+        id={id}
+        onChange={onChange}
+        checked={isChecked}
+        className={styles.box}
+      />
     </li>
   );
 }
